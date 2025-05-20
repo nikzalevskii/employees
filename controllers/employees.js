ee30cd41061dde293388bef52b929fd9ffbfec1b
Nikita Zalevskii <nikita.zalevsky@amigochat.io>
2025-05-20 21:22:59 +0300

    finished backend part. add edit, remove and get employee

--- controllers/employees.js
+++ controllers/employees.js
@@ -11,7 +11,7 @@ const all = async (req, res) => {
 
     res.status(200).json(employess);
   } catch (error) {
-    res.status(500).json({ message: "Не удалось получить сотрудников" });
+    returnres.status(500).json({ message: "Не удалось получить сотрудников" });
   }
 };
 
@@ -37,11 +37,78 @@ const add = async (req, res) => {
 
     return res.status(200).json(employee);
   } catch (error) {
-    res.status(500).json({ message: "Что-то пошло не так" });
+    returnres.status(500).json({ message: "Что-то пошло не так" });
+  }
+};
+
+/**
+ * @route POST /api/employees/remove/:id
+ * @desc Удаление сотрудника
+ * @access private
+ */
+const remove = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await prisma.employee.delete({
+      where: {
+        id,
+      },
+    });
+    return res.status(200).json({ message: "Ок. Сотрудник удален" });
+  } catch (error) {
+    return res.status(500).json({ message: "Не удалось удалить сотрудника" });
+  }
+};
+
+/**
+ * @route POST /api/employees/edit/:id
+ * @desc Редактирование сотрудника
+ * @access private
+ */
+const edit = async (req, res) => {
+  try {
+    const data = req.body;
+    const { id } = data;
+
+    await prisma.employee.update({
+      where: {
+        id,
+      },
+      data,
+    });
+
+    res.status(200).json({ message: "Ок. Сотрудник отредактирован" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Не удалось редактировать сотрудника" });
+  }
+};
+
+/**
+ * @route GET /api/employees/:id
+ * @desc Получение сотрудника
+ * @access private
+ */
+const employee = async (req, res) => {
+  // get запрос. берем из параметров
+  const { id } = req.params;
+  try {
+    const employee = await prisma.employee.findUnique({
+      where: { id },
+    });
+
+    res.status(200).json(employee);
+  } catch (error) {
+    return res.status(500).json({ message: "Не удалось получить сотрудника" });
   }
 };
 
 module.exports = {
   all,
   add,
+  remove,
+  edit,
+  employee,
 };
